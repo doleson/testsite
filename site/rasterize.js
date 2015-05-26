de60543e90c7edeb6082390bf5f0fe4e9d6ecec1
Dave Oleson <oleson@gmail.com>
2015-05-26 00:57:31 -0700

    change rasterize time

--- site/rasterize.js
+++ site/rasterize.js
@@ -46,7 +46,7 @@ if (system.args.length < 3 || system.args.length > 5) {
             window.setTimeout(function () {
                 page.render(output);
                 phantom.exit();
-            }, 150);
+            }, 1000);
         }
     });
 }
